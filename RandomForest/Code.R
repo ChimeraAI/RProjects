ab0296aeb964d3d680dd11b515a545bc980cab66
JamesXiao <yimingxiao66@gmail.com>
2017-08-28 14:03:04 -0400

    Created first draft of the xgboost code

--- RandomForest/Code.R
+++ RandomForest/Code.R
@@ -1,55 +1,34 @@
-# Implementation of xgboost
-# Load libraries ---------------------------------------
-library(caret)
+library(data.table)
+
+#Read and separate raw data
+rawData <- read.csv("bank-full.csv", sep = ";", header = TRUE)
+
+#Convert to DataFrame
+setDT(rawData)
+
+#Set seed
+set.seed(11)
+
+#Shuffle Data
+rawData <- rawData[sample(nrow(rawData)),]
+
+#Split rawData into training and testing data
 library(caTools)
+tempData <- sample.split(rawData$y, SplitRatio = 0.7)
+trainData <- rawData[tempData]
+testData <- rawData[!tempData]
+
+tr_target <- trainData$y
+test_target <- testData$y
+
+new_tr <- model.matrix(~.+0, data = trainData[,-c("y")])
+new_test <- model.matrix(~.+0, data = testData[,-c("y")])
+
+tr_target <- as.numeric(tr_target) - 1
+test_target <- as.numeric(test_target) - 1
+
 library(xgboost)
-library(Matrix)
-library(car)
+dTrain <- xgb.DMatrix(data = new_tr, label = tr_target)
+dTest <- xgb.DMatrix(data = new_test, label = test_target)
 
-dataFrame <- read.csv("bank-full.csv", sep = ";", header = TRUE)
-
-dataFrame$day <- NULL
-
-dataFrame$y <- as.numeric(dataFrame$y)
-
-ohe_feats = c("job","marital","education","default","housing","loan","contact","poutcome","month")
-
-dummies <- dummyVars(~ job + marital + education + default + housing + loan + contact + poutcome + month, data = dataFrame)
-
-df_all_ohe <- data.frame(predict(dummies, newdata = dataFrame))
-
-df_all_combined <- cbind(dataFrame[,-c(which(colnames(dataFrame) %in% ohe_feats))],df_all_ohe)
-
-# Shuffle data
-df_all_combined <- df_all_combined[sample(nrow(df_all_combined)),]
-
-# Split data
-tempData <- sample.split(df_all_combined$y, SplitRatio = 0.7)
-trainData <- df_all_combined[tempData,]
-testData <- df_all_combined[!tempData,]
-
-drop <- ("y")
-
-xgb <- xgboost(data = data.matrix(trainData[,!(names(trainData) %in% drop)]), 
-               label = trainData$y, 
-               eta = 0.1,
-               max_depth = 15, 
-               nround=25, 
-               subsample = 0.5,
-               colsample_bytree = 0.5,
-               seed = 1,
-               eval_metric = "merror",
-               objective = "multi:softprob",
-               num_class = 51,
-               nthread = 3
-)
-
-y_pred <- predict(xgb, data.matrix(trainData[,!(names(trainData) %in% drop)]))
-
-
-
-
-
-
-
-
+params <- list(booster = "gbtree", objective = "binary:logistic", eta=0.3, gamma=0, max_depth=6, min_child_weight=1, subsample=1, colsample_bytree=1)
