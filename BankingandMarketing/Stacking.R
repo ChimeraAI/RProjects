a2aabcd7ab9a4704be3d43f7b4dddd21e49b1708
ChimeraAI <yimingxiao66@gmail.com>
2017-09-01 10:32:00 -0400

    Turning stacking code into a function.

--- BankingandMarketing/Stacking.R
+++ BankingandMarketing/Stacking.R
@@ -7,34 +7,38 @@ library(caTools)
 library(parallel)
 library(parallelMap)
 
-#Read and separate raw data
-rawData <- read.csv("BankingandMarketing/bank-full.csv", sep = ";", header = TRUE)
+stackingFunc <- function(rawData) {
 
-#Convert to DataFrame
-as.data.frame(rawData) 
+  #Convert to DataFrame
+  as.data.frame(rawData) 
+  
+  # Convert characters to factors
+  fact_col <- colnames(rawData)[sapply(rawData,is.character)]
+  
+  for(i in fact_col) set(rawData,j=i,value = factor(rawData[[i]]))
+  
+  # Create Task
+  tsk <-  makeClassifTask(data = rawData, target = "y")
+  
+  # Perform one hot encoding
+  tsk <- createDummyFeatures (obj = tsk)
+  
+  
+  base = c("classif.xgboost", "classif.randomForest")
+  lrns = lapply(base, makeLearner)
+  lrns = lapply(lrns, setPredictType, "prob")
+  m = makeStackedLearner(base.learners = lrns,
+                         predict.type = "prob", method = "hill.climb")
+  
+  parallelStartSocket(cpus = detectCores())
+  
+  tmp = train(m, tsk)
+  res = predict(tmp, tsk)
+  
+  parallelStop()
+  
+  return (performance(res, measures = acc))
 
-fact_col <- colnames(rawData)[sapply(rawData,is.character)]
 
-for(i in fact_col) set(rawData,j=i,value = factor(rawData[[i]]))
 
-#Set seed
-set.seed(11)
-
-tsk <-  makeClassifTask(data = rawData, target = "y")
-
-tsk <- createDummyFeatures (obj = tsk)
-
-base = c("classif.xgboost", "classif.randomForest")
-lrns = lapply(base, makeLearner)
-lrns = lapply(lrns, setPredictType, "prob")
-m = makeStackedLearner(base.learners = lrns,
-                       predict.type = "prob", method = "hill.climb")
-
-parallelStartSocket(cpus = detectCores())
-
-tmp = train(m, tsk)
-res = predict(tmp, tsk)
-
-parallelStop()
-
-performance(res, measures = acc)
+}
---
+++ main.R
@@ -0,0 +1,23 @@
+#Load libraries
+library(data.table)
+library(caret)
+library(xgboost)
+library(mlr)
+library(caTools)
+library(parallel)
+library(parallelMap)
+
+# Source functions from other files
+source("Stacking.R")
+source("Boosting.R")
+source("Bagging.R")
+
+#Read and separate raw data
+rawData <- read.csv("BankingandMarketing/bank-full.csv", sep = ";", header = TRUE)
+
+#Convert to DataFrame
+setDT(rawData)
+
+#Set seed
+set.seed(11)
+
